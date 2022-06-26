52771db60d25a81f2dac0cd8932487309430331d
memmynn <mehmetuyarwww@hotmail.com>
2022-06-27 00:08:46 +0300

    .

--- src/index.js
+++ src/index.js
@@ -603,13 +603,14 @@ class playLevel extends Phaser.Scene{
 
 
 titleScene.preload = function() {
-    this.load.image('background', 'src/assets/ui/BG.png');
+    this.load.image('background0', 'src/assets/ui/bg0.png');
 
     //this.load.image('logo', require('./assets/logo.png'));
     // load assets needed in our game
+    this.load.image('blueButton3', 'src/assets/ui/Button_13.png');
   this.load.image('blueButton1', 'src/assets/ui/blue_button02.png');
   this.load.image('blueButton2', 'src/assets/ui/blue_button03.png');
-  //this.load.image('phaserLogo', 'src/assets/logo.png');
+  this.load.image('logo', 'src/assets/slidyPuzz.png');
   this.load.image('box', 'src/assets/ui/grey_box.png');
   this.load.image('checkedBox', 'src/assets/ui/blue_boxCheckmark.png');
   this.load.audio('bgMusic', ['src/assets/TownTheme.mp3']);
@@ -618,32 +619,31 @@ titleScene.preload = function() {
 
 titleScene.create = function() {
 
-    this.add.image(400, 300, "background");
-    const x = this.scale.width * 0.1
-		const y = this.scale.height * 0.2
-
-		this.add.text(x, y, 'slidy puzz', {
-			fontFamily: 'cursive',
-			fontSize: '128px',
-			color: '#fff',
-			fontStyle: 'normal',
-			stroke: '#374F9D',
-			strokeThickness: 30
-		});
+    this.add.image(400, 300, "background0");
+    const x = this.scale.width * 0.5;
+		const y = this.scale.height * 0.25;
+        this.add.image(x, y, "logo");
 	
 
     //var bg = this.add.sprite(0,0,'logo');
     //bg.setOrigin(0,0);
+    this.startButton = this.add.sprite(400, 350, 'blueButton3').setScale(0.5).
+    setInteractive({ useHandCursor: true });
 
-    var text = this.add.text(300, 350, 'START!', {  fontFamily: 'cursive', fontSize: '55px', color: "white"});
-    text.setInteractive({ useHandCursor: true });
-    text.on('pointerdown', () => clickButton());
-    function clickButton() {
+   this.text = this.add.text(300, 350, 'sTaRt!', {  fontFamily: 'cursive', fontSize: '38px', color: "white"});
+    
+    Phaser.Display.Align.In.Center(this.text, this.startButton);
+    this.startButton.on('pointerdown', () => clickButton());
+function clickButton() {
         titleScene.scene.start('PlayGame');
     }
+    
 
-    this.optionButton = this.add.sprite(400, 500, 'blueButton1').setInteractive();
-    this.optionText = this.add.text(0, 0, 'Options', { fontSize: '32px', fill: '#000000' });
+
+    this.optionButton = this.add.sprite(400, 450, 'blueButton3').
+    setScale(0.5).
+    setInteractive({ useHandCursor: true });
+    this.optionText = this.add.text(0, 0, 'oPtiOns', {  fontFamily: 'cursive', fontSize: '38px', color: "white"});
     Phaser.Display.Align.In.Center(this.optionText, this.optionButton);
 
     this.optionButton.on('pointerdown', function (pointer) {
@@ -651,8 +651,9 @@ titleScene.create = function() {
       this.scene.start('Options');
     }.bind(this));
 
-    this.creditsButton = this.add.sprite(400, 550, 'blueButton1').setInteractive();
-    this.creditsText = this.add.text(0, 0, 'Credits', { fontSize: '32px', fill: '#000000' });
+    this.creditsButton = this.add.sprite(400, 550, 'blueButton3').setScale(0.5).
+    setInteractive({ useHandCursor: true });
+    this.creditsText = this.add.text(0, 0, 'cReditS', {  fontFamily: 'cursive', fontSize: '38px', color: "white"});
     Phaser.Display.Align.In.Center(this.creditsText, this.creditsButton);
 
     this.creditsButton.on('pointerdown', function (pointer) {
